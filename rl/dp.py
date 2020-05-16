57c5dd5ae42089d1bd5dfaf0437ab02a7434f8bf
jithin kumar <44967770+jithin-pal@users.noreply.github.com>
2020-05-17 00:11:00 +0530

    Add files via upload

--- rl/dp.py
+++ rl/dp.py
@@ -38,7 +38,31 @@ def policy_eval(env, policy, gamma=1, theta=1e-8):
 
 random_policy = np.ones([sp,a_s])/a_s
 V = policy_eval(env, random_policy)
-plot_values(V)
+
+#Action value function
+def action_value_function(env, V, s, gamma=0.9):
+    q = np.zeros(env.nA)
+    for a in range(env.nA):
+        for prob, next_state, reward, done in env.P[s][a]:
+            q[a] += prob * (reward + gamma * V[next_state])
+    return q
+
+Q = np.zeros([sp, a_s])
+for s in range(sp):
+    Q[s] = action_value_function(env, V, s)
+print(Q)
+
+def new_policy(env, V, gamma=1):
+    policy = np.zeros([env.nS, env.nA]) / env.nA
+    for s in range(env.nS):
+        q = action_value_function(env, V, s, gamma)
+        best_a = np.argwhere(q==np.max(q)).flatten()
+        policy[s] = np.sum([np.eye(env.nA)[i] for i in best_a], axis=0)/len(best_a)
+    return policy
+policy = new_policy(env, V) 
+print('/n new policy is:', policy, 'L=0, R = 2, U= 3, D= 1')
+
+
 
 
 
